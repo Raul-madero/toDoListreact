eed0c9f2f0f7b3fe7fbe73a8147d48726c1b10c1
Raul Madero <r.madero.ramirez@gmail.com>
2023-11-18 12:57:36 -0600

    Se creo e componente Item

--- src/js/component/ToDo.jsx
+++ src/js/component/ToDo.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
+import Item from "./Item";
 
 const ToDo = () => {
   const [inputValue, setInputValue] = useState("");
   const [item, setItem] = useState([]);
-  const [showButton, setShowButton] = useState(false);
 
   return (
     <>
@@ -24,29 +24,7 @@ const ToDo = () => {
               }}
             ></input>
           </li>
-          {item.map((task, index) => (
-            <li
-              onClick={(e) => setShowButton(true)}
-              onMouseOut={(e) => setShowButton(false)}
-              id={index}
-              className="bg-light list-group-item border-0 my-2 border-bottom border-success d-flex justify-content-between"
-              key={index}
-            >
-              {task}
-              {showButton ? (
-                <button
-                  className="btn btn-danger"
-                  onClick={() =>
-                    setItem(
-                      item.filter((t, currentIndex) => index != currentIndex)
-                    )
-                  }
-                >
-                  X
-                </button>
-              ) : null}
-            </li>
-          ))}
+          <Item task={item} />
         </ul>
         <div>
           <p className="text-start border-secondary bg-secondary bg-opacity-50 p-2 my-2 w-50 fw-lighter fst-italic">
